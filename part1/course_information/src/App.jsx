a86a8928f6123976516d3f6540b91d194aedfbc9
Sami Räihä <saraiha@helsinki.fi>
2026-06-01 20:11:11 +0300

    exercise part 1.3

--- part1/course_information/src/App.jsx
+++ part1/course_information/src/App.jsx
@@ -1,49 +1,56 @@
 
 const Header = (props) => {
-
+  //console.log('header',props)
   return (
     <h1>{props.course}</h1>
   )
 }
-
 const Part = (props) =>{
 
   return(
-    <p>{props.part} {props.exercise}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
 const Constent = (props) => {
-
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
+      <Part part={props.part1}/>
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
     </>
   )
-}
- 
+  }
+  
 const Total = (props) =>{
+  console.log('total',props)
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course}/>
-      <Constent part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Constent part1={part1} part2={part2} part3={part3} />
+      <Total  part1={part1} part2={part2} part3={part3}/>
+     {/*
+      */}
     </>
   )
 }
