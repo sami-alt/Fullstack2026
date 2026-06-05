04b4ac3f6fcf5683463121886a379ff89739d0b2
Sami Räihä <saraiha@helsinki.fi>
2026-06-05 10:47:57 +0300

    exrcise part 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,10 +9,13 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+ 
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {setGood(good + 1), setAll(all + 1)}
+  const handleNeutral = () => {setNeutral(neutral + 1), setAll(all + 1)}
+  const handleBad = () => {setBad(bad + 1), setAll(all + 1)}
+  const handleAll = () => setAll(all + 1)
 
   return (
     <div>
@@ -24,6 +27,9 @@ const App = () => {
       <Disaplay text={"good"} value={good}></Disaplay>
       <Disaplay text={"neutral"} value={neutral}></Disaplay>
       <Disaplay text={"bad"} value={bad}></Disaplay>
+      <Disaplay text={"all"} value={good + bad + neutral}></Disaplay>
+      <Disaplay text={"average"} value={all == 0 ? 0 : (1*good + 0*neutral + -1*bad)/all}></Disaplay>
+      <Disaplay text={"positive"} value={all == 0 ? 0 :(good/all)*100 + " %"}></Disaplay>
     </div>
   )
 }
