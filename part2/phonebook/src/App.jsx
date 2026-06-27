0229d5e8fc8b9a489f15c2124a52962db521f040
Sami Räihä <saraiha@helsinki.fi>
2026-06-27 11:15:58 +0300

    exercises part 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Contacts from './components/contacts'
 import Filter from './components/filter'
 import PersonForm from './components/addPerson'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredNames, setFilteredNames] = useState([])
   const [show, setShow] = useState(false)
-  //const [nameToSearch, setNameToSearch] = useState(false)
+  
+  useEffect(()=> {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(res => setPersons(res.data))
+  },[])
+  
 
 
- 
   return (
     <div>
       <h2>Phonebook</h2>
