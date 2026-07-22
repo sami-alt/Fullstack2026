5e4b62ba4efc1949e7ae60d0a9dda67551c1d730
Sami Räihä <saraiha@helsinki.fi>
2026-07-22 12:31:39 +0300

    exercises part 4.2-4.3

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -1,7 +1,22 @@
-const app = require('./app') // the actual Express application
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+const app = express()
+
+const mongoUrl = process.env.MONGO_DB_URI
+
+mongoose.connect(mongoUrl, { family: 4 })
+    .then(() => logger.info("Connected to MongoDB"))
+    .catch(error => logger.error(error))
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+ 
+
+
+module.exports = app
--- part4/Blog/utils/config.js
+++ part4/Blog/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGO_DB_URI = process.env.MONGO_DB_URI
 
-module.exports = { MONGODB_URI, PORT }
+module.exports = { MONGO_DB_URI, PORT }
