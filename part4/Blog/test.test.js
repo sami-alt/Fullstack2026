bd0c55e1f695544d398cdd895e9837ad73e55a4a
Sami Räihä <saraiha@helsinki.fi>
2026-07-22 13:41:14 +0300

    exercise part 4.5

--- part4/Blog/test.test.js
+++ part4/Blog/test.test.js
@@ -53,3 +53,45 @@ describe("likes", ()=> {
   })
   
 })
+
+
+describe("popular", () => {
+  const testBlogs = [
+    {
+      _id:"1q2w3e4r5t6y",
+      title:"Test for blog apps",
+      author:"B. Virtanen",
+      url:"www.bvirta.fi",
+      likes:67,
+      _v:0
+    },
+    {
+      _id:"0o9i8u7y6t5",
+      title:"Apps for blogs tests",
+      author:"Millen Sapkowski",
+      url:"www.websyte.to",
+      likes:5,
+      _v:0
+    },
+    {
+      _id:"12o3i44y6t",
+      title:"test blogs for blog test",
+      author:"Touko Mäkinen",
+      url:"www.net.net",
+      likes:34,
+      _v:0
+    },
+    {
+      _id:"1pq02ow93ie8u4r7",
+      title:"blog test for test blogs",
+      author:"A.U Thor",
+      url:"www.valhalla.nor",
+      likes:123,
+      _v:0
+    }
+  ]
+  test('most likes for blog post', () => {
+    const result = listHelper.favoriteBlog(testBlogs)  
+    assert.strictEqual(result.title, "blog test for test blogs")
+  })
+})
--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -2,14 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
-
 const totalLikes = (blogs) => {
   const likes = blogs.reduce((acc, blog) =>  acc + blog.likes, 0)
   return likes
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((prev, curr) =>  prev.likes > curr.likes ? prev : curr)
+  return favorite
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
