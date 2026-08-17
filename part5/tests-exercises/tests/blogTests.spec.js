b67a241c04dbbc31a2c7120d15d8d82d26a52b22
Sami Räihä <saraiha@helsinki.fi>
2026-08-17 12:11:02 +0300

    exercise part 5.19

--- part5/tests-exercises/tests/blogTests.spec.js
+++ part5/tests-exercises/tests/blogTests.spec.js
@@ -38,3 +38,33 @@ describe('Login tests', () => {
   })
     
 })
+
+describe('logged in user functionalities', ()=> {
+  beforeEach(async ( {page, request} ) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Testi Tahvo',
+        username: 'tester1',
+        password: 'test'
+      }
+    })
+ 
+    await page.goto('/')
+  })
+  test('user can add blog and it becomes visible in list of blogs',  async({page})=> {
+    await page.getByRole('textbox').first().fill('tester1')
+    await page.getByRole('textbox').last().fill('test')
+    await page.getByRole('button', {name:'login'}).click()
+    await page.getByRole('button', {name:'Add blog post'}).click()
+    await page.locator('#title').fill('Test of blogs')
+    await page.locator('#author').fill('Testter of blogs')
+    await page.locator('#url').fill('florist.com')
+    await page.getByRole('button', {name:'Add post'}).click()
+
+    const newPost = page.getByText('Test of blogs')
+    await expect(newPost).toBeVisible()
+  })
+
+
+})
